9d15fc963a3cca048c40db38f0a5dc3b77ac85a4
HenriqueCosta05 <Henriquebenedictocostadev@gmail.com>
2025-02-11 09:31:29 -0300

    feat: kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk

--- components/Accordion/Accordion.styles.ts
+++ components/Accordion/Accordion.styles.ts
@@ -1,18 +1,24 @@
 import styled from "styled-components";
-import { Accordion as MuiAccordionContainer, AccordionSummary as MuiAccordionSummary, AccordionDetails as MuiAccordionDetails} from "@mui/material"; 
+import { Accordion as MuiAccordionContainer, AccordionSummary as MuiAccordionSummary, AccordionDetails as MuiAccordionDetails, Paper} from "@mui/material"; 
 import { theme } from "../../theme/styled-components";
 
 export const StyledAccordionContainer = styled(MuiAccordionContainer)`
-    background-color: ${theme.palette.primary.dark}
-    color: ${theme.palette.primary.contrastText}
+    & .MuiAccordionSummary-root {
+        font-family: "Red Hat Display", Arial, sans-serif;
+        background-color: ${theme.palette.background[700]};
+        color: ${theme.palette.background[50]};
+        border-radius: 5px 5px 0 0;
+    }
 `
 
 export const StyledAccordionSummary = styled(MuiAccordionSummary)`
-    background-color: ${theme.palette.primary.dark}
-    color: ${theme.palette.primary.contrastText}
+    color: ${theme.palette.primary.contrastText};
 `
 
 export const StyledAccordionDetails = styled(MuiAccordionDetails)`
-    background-color: ${theme.palette.primary.dark}
-    color: ${theme.palette.primary.contrastText}
+    font-family: "Red Hat Display", Arial, sans-serif;
+    background-color: ${theme.palette.background[700]};
+    border-top: 1px solid #fff;
+    border-radius: 0 0 5px 5px;
+    color: ${theme.palette.background[50]};
 `
